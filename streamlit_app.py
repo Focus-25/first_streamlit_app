6d0ce76bf48ef2516510af88edae6914e2fdd1d2
Focus-25 <100894512+Focus-25@users.noreply.github.com>
2024-04-26 11:25:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ else:
     st.error(f"Failed to fetch data for the fruit: {fruit_choice}")
 
 # Superset Embedding
-st.header("Superset Report")
+st.header("Bclever Report")
 superset_html = """
 <iframe width="600" height="400" src="https://superset.apache.org//view?r=eyJrIjoiMmIxNGNlMWMtNjRiMC00MGM3LTg4MjctM2RiYzY0NDI0ZTY0IiwidCI6IjViNDRjYjQyLTBlNGItNGI1ZS05NDA4LTFjZjFiMDc0ZGViMiIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
 """
